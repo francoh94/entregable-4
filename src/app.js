ec10d1cd30f35a26294ed32aaac3849d7c565d10
preentrega 2-hornos <francohornos@outlook.es>
2023-08-06 11:22:54 -0300

    entregable4

--- src/app.js
+++ src/app.js
@@ -4,14 +4,16 @@ import handlebars from 'express-handlebars';
 import viewsRouter from './routes/views.routes.js'
 import { Server} from "socket.io";
 
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(__dirname +'/public'))
 
+
 app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname +'/views/')
+app.set('views', __dirname +'/views')
 app.set('view engine','handlebars')
 
 app.use('/api/views', viewsRouter)
@@ -25,6 +27,22 @@ const httpServer = app.listen(PORT,()=>{
 
 const socketServer = new Server(httpServer)
 
+const productos =[]
+
 socketServer.on('connection', (socket)=>{
     console.log('cliente conectado');
+    socket.emit('todoslosproductos', productos);
+
+socket.on('producto',(producto)=>{
+    productos.push({id:socket.id,producto})
+    socketServer.emit('todoslosproductos',productos)
+  })
+  socket.on('eliminarProducto', (productoAEliminar) => {
+    const indiceProducto = productos.findIndex(obj => obj.producto === productoAEliminar);
+    if (indiceProducto !== -1) {
+        productos.splice(indiceProducto, 1);
+        socketServer.emit('todoslosproductos', productos);
+    }
+});
+
 })
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,31 @@
 const socketClient = io()
+
+const formulario = document.getElementById('formulario')
+const productoInput = document.getElementById('producto')
+const divproductos = document.getElementById('divproductos');
+
+formulario.onsubmit = (e)=>{
+    e.preventDefault()
+    socketClient.emit('producto',productoInput.value)
+    
+}
+socketClient.on('todoslosproductos',(productos)=>{
+    const todoslosproductos = productos.map(objproductos=>{
+        return `
+          <div>
+            <p>${objproductos.producto}</p>
+            <button class="eliminar-btn" data-producto="${objproductos.producto}">Eliminar</button>
+          </div>
+        `;
+    }).join('');
+    divproductos.innerHTML = todoslosproductos;
+
+    // Agregar un listener para los botones de eliminar
+    const eliminarBtns = document.querySelectorAll('.eliminar-btn');
+    eliminarBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const productoAEliminar = e.target.getAttribute('data-producto');
+            socketClient.emit('eliminarProducto', productoAEliminar);
+        });
+    });
+});
